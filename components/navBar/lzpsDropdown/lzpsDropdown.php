e13f9956fc8775ea0233bc2b6123c0ec0fb4bdeb
mike14747 <mike4747@oh.rr.com>
2019-10-14 23:13:44 -0400

    changed the name of the lzp folder and db table to lzps, adjusted references to them in lzpsDropdown.php, added a config folder with a db schema file

--- components/navBar/lzpsDropdown/lzpsDropdown.php
+++ components/navBar/lzpsDropdown/lzpsDropdown.php
@@ -1,10 +1,10 @@
 <?php
-$query_lzps = $conn->query("SELECT * FROM lzp ORDER BY year ASC");
+$query_lzps = $conn->query("SELECT * FROM lzps ORDER BY year DESC");
 if ($query_lzps->num_rows > 0) {
     echo '<li><a href="javascript:void(0)">LZP Archive +</a>';
     echo '<ul>';
     while ($result_lzps = $query_lzps->fetch_assoc()) {
-        echo '<li><a href="lzp/' . rawurlencode($result_lzps['file_name']) . '">' . $result_lzps['navbar_name'] . '</a></li>';
+        echo '<li><a href="lzps/' . rawurlencode($result_lzps['file_name']) . '">' . $result_lzps['navbar_name'] . '</a></li>';
     }
     echo '</ul>';
     echo '</li>';
