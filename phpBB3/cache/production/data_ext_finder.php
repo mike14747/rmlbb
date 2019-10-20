475f17f38f4e17596800d2a4c8cf8debafdb723d
mike14747 <mike4747@oh.rr.com>
2019-10-20 19:55:58 -0400

    removed the phpBB3 folder from .gitignore and temporarily removed it from the folder so it'll be deleted from the repo in this commit

--- phpBB3/cache/production/data_ext_finder.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1602437693
-287
-a:1:{s:32:"634865559a7118e363ac18e94bf4055a";a:1:{i:0;a:4:{s:10:"named_path";s:57:"ext/phpbbmodders/adduser/language/en/info_acp_adduser.php";s:8:"ext_name";s:20:"phpbbmodders/adduser";s:4:"path";s:37:"ext/phpbbmodders/adduser/language/en/";s:8:"filename";s:20:"info_acp_adduser.php";}}}
